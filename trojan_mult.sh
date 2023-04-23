d17efa710b847da6afb497507bb9dc2032589c99
guolongchu <guolongchu@gmail.com>
2023-04-23 11:56:48 +0800

    Update trojan_mult.sh

--- trojan_mult.sh
+++ trojan_mult.sh
@@ -182,6 +182,7 @@ EOF
         "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
         "sni": "",
         "alpn": [
+            "h3",
             "h2",
             "http/1.1"
         ],
@@ -216,6 +217,8 @@ EOF
         "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
         "prefer_server_cipher": true,
         "alpn": [
+            "h3"
+            "h2"
             "http/1.1"
         ],
         "reuse_session": true,
